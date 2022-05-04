0096cf1d92f40dfd82cee07895970feb08ae85c1
De221 <denits221@abv.bg>
2022-05-04 18:25:49 +0300

    admin-page-added

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
 function buttonLogin() {
     window.location.href = 'http://localhost:8080/login';
 }
+jQuery( "li:has(ul)" ).hover(function(){ // When a li that has a ul is clicked ...
+	jQuery(this).toggleClass('active');}); // then toggle (add/remove) the class 'active' on it. 
