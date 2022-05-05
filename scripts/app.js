6a9ddba4df77a8ba115ef0335cf79cd5b65b40de
De221 <denits221@abv.bg>
2022-05-05 17:00:25 +0300

    conn1

--- scripts/app.js
+++ scripts/app.js
@@ -8,17 +8,22 @@ jQuery( "li:has(ul)" ).hover(function(){ // When a li that has a ul is clicked .
 function fetchWarehouses() 
 {
     var listCityNames = [];
-    function putWarehouses(length, spantext) {
+    var listWarehouseStorages = [];
+    function putWarehouses(length, spantext, circleText) {
         for (let i = 0; i < length; i++) {
             var div = document.createElement("div");
             div.className="warehouse__icon";
             var img = document.createElement("img");
             img.setAttribute("src", "https://cdn-icons-png.flaticon.com/512/189/189214.png");
+            var infoCircle = document.createElement("div");
+            infoCircle.className="quick__info__circle";
+            infoCircle.innerHTML="Storage:" + circleText[i];
             var span = document.createElement("span");
             span.className="warehouse__icon__text";
             span.textContent=spantext[i];
 
             div.appendChild(img);
+            div.appendChild(infoCircle);
             div.appendChild(span);
 
             var block = document.getElementById("index-page");
@@ -30,15 +35,15 @@ function fetchWarehouses()
     .then(response => {return response.json();})
     .then(json => 
         { 
-            console.log(json); 
-            console.log(json.length);
-
+            //console.log(json); 
+            //console.log(json.length);
             for (var warehouse of json) {
                 //console.log(warehouse);--> full info
                 listCityNames.push(warehouse['cityName']);
+                listWarehouseStorages.push(warehouse['storage_space']);
             }
-
-            putWarehouses(json.length, listCityNames);
+            console.log(listWarehouseStorages);
+            putWarehouses(json.length, listCityNames, listWarehouseStorages);
         })
     
 }
