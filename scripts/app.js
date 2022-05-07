cda695fc02be39269fec525dbd49b7cfcfd80275
De221 <denits221@abv.bg>
2022-05-07 18:03:56 +0300

    gdg

--- scripts/app.js
+++ scripts/app.js
@@ -156,10 +156,10 @@ function isLoged() {
     var block2 = document.getElementById("index-user-name");
     block2.style.display = "flex";
     //block2.setAttribute('color', 'white');
-    if(block2.textContent.startsWith('Admin'))
-      {block2.setAttribute('href', 'https://de221.github.io/DNDWarehouse-Frontend/admin-home')};
-    if(block2.textContent.startsWith('User'))
-      {block2.setAttribute('href', 'https://de221.github.io/DNDWarehouse-Frontend/user-home')};
+    // if(block2.textContent.startsWith('Admin'))
+    //   {block2.setAttribute('href', 'https://de221.github.io/DNDWarehouse-Frontend/admin-home')};
+    // if(block2.textContent.startsWith('User'))
+    //   {block2.setAttribute('href', 'https://de221.github.io/DNDWarehouse-Frontend/user-home')};
   }   
 }
     // async function loadIntoTable() {
