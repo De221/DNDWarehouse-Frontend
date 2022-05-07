5142a0041992c22bde80b6c1d8c1923e50b48563
De221 <denits221@abv.bg>
2022-05-07 16:09:35 +0300

    logina bace

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,61 @@
-function buttonLogin() {
-    window.location.href = 'http://localhost:8080/login';
+function login() {
+  let Role = "";
+  var email = document.getElementsByName("email")[0].value;
+  var password = document.getElementsByName("password")[0].value;
+  
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json');
+  const jwtResponse = fetch('http://localhost:8080/authenticate',
+  {
+    method: 'POST',
+    headers: myHeaders,
+    body: JSON.stringify({"username": email, "password": password})
+  })
+  .then(response => response.text())
+    .then(text => 
+      {
+        if(text.startsWith("{\"jwt\":"))
+        {
+          $('#alert1').hide('fade') // close alert incorrect login info
+          str1 = text.substring(8);
+          str2 = str1.slice(0, -2);
+          jwtToken = "Bearer " + str2;
+
+          localStorage.setItem('jwtToken', jwtToken); // write
+          console.log(localStorage.getItem('jwtToken')); // read
+          $('#loginModal').modal('hide');
+
+
+          const myHeaders1 = new Headers();
+          myHeaders1.append('Authorization', jwtToken);
+          fetch('http://localhost:8080/currentUser/getRole',
+          {
+            method: 'GET',
+            headers: myHeaders1,
+          })
+          .then(response => response.text())
+          .then(text => 
+              { 
+                Role = text;
+                if (Role.startsWith('[ROLE_ADMIN]'))
+                {window.location.href = 'https://de221.github.io/DNDWarehouse-Frontend/admin-home'}
+                else if (Role.startsWith('[ROLE_USER]'))
+                {window.location.href = 'https://de221.github.io/DNDWarehouse-Frontend/user-home'};
+              });
+        }
+        else
+        {
+          $('.alert').show('fade');
+          //$('.alert').css("display","block"); 
+        }
+      })
 }
+$('#link-close').click(function(){$('#alert1').hide('fade');}); // close alert incorrect login info
 
 jQuery( "li:has(ul)" ).hover(function(){ // When a li that has a ul is clicked ...
 	jQuery(this).toggleClass('active');}); // then toggle (add/remove) the class 'active' on it. 
 
-function fetchWarehouses() 
+async function fetchWarehouses() 
 {
     var listCityNames = [];
     var listWarehouseStorages = [];
@@ -31,7 +81,7 @@ function fetchWarehouses()
         };
     }
     
-    let json = fetch('http://localhost:8080/packet/fetchWarehouses')
+    let json = await fetch('http://localhost:8080/packet/fetchWarehouses')
     .then(response => {return response.json();})
     .then(json => 
         { 
@@ -42,17 +92,124 @@ function fetchWarehouses()
                 listCityNames.push(warehouse['cityName']);
                 listWarehouseStorages.push(warehouse['storage_space']);
             }
-            console.log(listWarehouseStorages);
+            //console.log(listWarehouseStorages);
             putWarehouses(json.length, listCityNames, listWarehouseStorages);
-        })
-    
+        })   
 }
-function getUserInfo() {
-    let json = fetch('http://localhost:8080/currentUser/username')
-    .then(response => response.text())
-    .then(text => 
-        { 
-            console.log(text);
-        })
+    function getUserInfo() {
+      const myHeaders = new Headers();
+      myHeaders.append('Authorization', localStorage.getItem('jwtToken'))
+
+      fetch('http://localhost:8080/currentUser/getRole',
+      {
+        method: 'GET',
+        headers: myHeaders,
+      })
+      .then(response => response.text())
+      .then(text => 
+          { 
+            if(text.startsWith('[ROLE_ADMIN]'))
+            text1 = "Admin ";
+            if(text.startsWith('[ROLE_USER]'))
+            text1 = "User ";
+              var block = document.getElementById("user-name");
+              block.textContent += text1;
+
+              fetch('http://localhost:8080/currentUser/getUsername',
+              {
+                method: 'GET',
+                headers: myHeaders,
+              })
+              .then(response2 => response2.text())
+              .then(text2 => 
+                  { 
+                      var block = document.getElementById("user-name");
+                      block.textContent += text2;
+                  })
+          })
 }
+    // async function loadIntoTable() {
+    //     const tableHead = table.querySelector("thead");
+    //     const tableBody = table.querySelector("tbody");
+    //     const response = await fetch('http://localhost:8080/task/fetch');
+    //     const data = await response.json();
+    //     buildHtmlTable(data);
+    //     //clear the table
+    //     tableHead.innerHTML = "<tr></tr>";
+    //     tableBody.innerHTML = "";
+    //     //populate the headers
+    //     for(const headerText of headers) {
+    //         const headerElement = document.createElement("th");
+    //         headerElement.textContent = headerText;
+    //         tableHead.querySelector("tr").appendChild(headerElement);
+    //     }
+    //     for(const row of rows){
+    //         const rowElement = document.createElement("tr");
+
+    //         for(const cellText of row){
+    //             const cellElement = document.createElement("tr");
+
+    //             cellElement.textContent = cellText;
+    //             rowElement.appendChild(cellElement);
+    //         }
+
+    //         tableBody.appendChild(rowElement);
+    //     }
+
+    //     console.log(data);
+    // }
     
+    async function LoadTable() {
+      const myHeaders = new Headers();
+      myHeaders.append('Authorization', localStorage.getItem('jwtToken'));
+      let json = await fetch('http://localhost:8080/task/fetch',
+      {
+        method: 'GET',
+        headers: myHeaders,
+      });
+      //let json = [{"id":3,"name":"Task3","start":null,"employees":[],"packets":[],"fullStatusName":"New Task","expectedFinish":null,"cityName":null},{"id":4,"name":"Task4","start":null,"employees":[],"packets":[],"fullStatusName":"New Task","expectedFinish":null,"cityName":null},{"id":5,"name":"Task5","start":null,"employees":[],"packets":[],"fullStatusName":"New Task","expectedFinish":null,"cityName":null},{"id":1,"name":"Task1","start":"2022-01-10T23:20:58.986+00:00","employees":[],"packets":[{"id":10,"name":"packetX","weight":0.269,"warehouseId":1}],"fullStatusName":"Finished","expectedFinish":"2022-01-10T23:26:58.986+00:00","cityName":"Sofia"},{"id":2,"name":"Task2","start":"2022-01-10T23:25:58.897+00:00","employees":[],"packets":[{"id":4,"name":"Kutiq pulna s taini","weight":69.0,"warehouseId":2}],"fullStatusName":"Finished","expectedFinish":"2022-01-11T05:50:44.398+00:00","cityName":"Plovdiv"}]
+      document.body.appendChild(buildHtmlTable(json));
+    }
+    
+    var _table_ = document.createElement('table');
+    _tr_ = document.createElement('tr'),
+    _th_ = document.createElement('th'),
+    _td_ = document.createElement('td');
+    
+    
+    // Builds the HTML Table out of myList json data from Ivy restful service.
+    function buildHtmlTable(arr) {
+      var table = _table_.cloneNode(false),
+        columns = addAllColumnHeaders(arr, table);
+      for (var i = 0, maxi = arr.length; i < maxi; ++i) {
+        var tr = _tr_.cloneNode(false);
+        for (var j = 0, maxj = columns.length; j < maxj; ++j) {
+          var td = _td_.cloneNode(false);
+          var cellValue = arr[i][columns[j]];
+          td.appendChild(document.createTextNode(arr[i][columns[j]] || ''));
+          tr.appendChild(td);
+        }
+        table.appendChild(tr);
+      }
+      return table;
+    }
+    
+    // Adds a header row to the table and returns the set of columns.
+    // Need to do union of keys from all records as some records may not contain
+    // all records
+    function addAllColumnHeaders(arr, table) {
+      var columnSet = [],
+        tr = _tr_.cloneNode(false);
+      for (var i = 0, l = arr.length; i < l; i++) {
+        for (var key in arr[i]) {
+          if (arr[i].hasOwnProperty(key) && columnSet.indexOf(key) === -1) {
+            columnSet.push(key);
+            var th = _th_.cloneNode(false);
+            th.appendChild(document.createTextNode(key));
+            tr.appendChild(th);
+          }
+        }
+      }
+      table.appendChild(tr);
+      return columnSet;
+    }
