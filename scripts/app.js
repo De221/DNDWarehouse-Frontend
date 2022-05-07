e1a38596d9b95140fe1b55ec53472227d000e609
De221 <denits221@abv.bg>
2022-05-07 19:02:41 +0300

    aaa

--- scripts/app.js
+++ scripts/app.js
@@ -173,7 +173,7 @@ function isLoged() {
             if(text.startsWith('[ROLE_USER]'))
             text1 = "User ";
               localStorage.setItem('role', text1.slice(0, -1));
-              console.log(localStorage.getItem('role'));
+              console.log("aaa " + localStorage.getItem('role'));
 
               if(localStorage.getItem('role').localeCompare('Admin') == 0)
                 {block2.setAttribute('onclick', 'transferAdmin();');};
